2566c6a32a9af44bffdd55b72efc0c9ad67d18db
Abdelaziz Jail <azizjail2003@gmail.com>
2026-01-20 17:30:13 +0100

    Integrate Vercel Speed Insights

--- app.vue
+++ app.vue
@@ -17,10 +17,12 @@
     </footer>
     <ConfirmModal />
     <GlobalLoader />
+    <SpeedInsights />
   </div>
 </template>
 
 <script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 const { initTheme } = useTheme();
 const { initLocale, t } = useTranslation();
 
